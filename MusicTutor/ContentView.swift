d169cb678ddf55d5fcc58b98b5755cf7a2841a91
Dhriti-P-17 <dhritipesh@gmail.com>
2025-10-23 21:11:07 -0700

    Initial Commit

---
+++ MusicTutor/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MusicTutor
+//
+//  Created by Dhriti Peshkar on 10/23/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MusicTutor/MusicTutorApp.swift
@@ -0,0 +1,17 @@
+//
+//  MusicTutorApp.swift
+//  MusicTutor
+//
+//  Created by Dhriti Peshkar on 10/23/25.
+//
+
+import SwiftUI
+
+@main
+struct MusicTutorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
